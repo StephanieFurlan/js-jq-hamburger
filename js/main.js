8768b271d2784fa87df773aa41985bbee099a239
Stephanie <stephanie.furlan94@gmail.com>
2020-11-23 15:16:22 +0100

    fadeIn, show or .active css class

--- js/main.js
+++ js/main.js
@@ -1,7 +1,14 @@
 $(".header-right > a").click(function() {
-   $(".hamburger-menu").fadeIn();
+   // jquery function fadeIn or show
+   // $(".hamburger-menu").fadeIn();
+   // add css class active (display: block)
+   $(".hamburger-menu").addClass("active");
 });
 
 $(".close").click(function() {
-   $(".hamburger-menu").fadeOut();
+   // jquery function fadeOut or hide
+   // $(".hamburger-menu").fadeOut();
+   // remove css class active (display: block)
+   $(".hamburger-menu").removeClass("active");
+
 })
